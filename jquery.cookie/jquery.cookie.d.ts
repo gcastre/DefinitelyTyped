f77ee35b1cd189c2b45d0131d97eba982f42458a
Grégoire Castre <castre@doxense.com>
2015-03-18 14:25:20 +0100

    Use union type for the expires and add some doc

--- jquery.cookie/jquery.cookie.d.ts
+++ jquery.cookie/jquery.cookie.d.ts
@@ -5,11 +5,32 @@
 
 ///<reference path="../jquery/jquery.d.ts" />
 
-
 interface JQueryCookieOptions {
-    expires?: any;
+	/** 
+	 *  Define lifetime of the cookie.
+	 *  Value can be a Number which will be interpreted as days from time of creation or a Date object.
+	 *	If omitted, the cookie becomes a session cookie.
+	 */
+    expires?: number|Date;
+	/** 
+	 *  Define the path where the cookie is valid.
+	 *  By default the path of the cookie is the path of the page where the cookie was created (standard browser behavior).
+	 *  If you want to make it available for instance across the entire domain use path: '/'.
+	 *  Default: path of page where the cookie was created.
+	 *  Note regarding Internet Explorer:
+	 *  Due to an obscure bug in the underlying WinINET InternetGetCookie implementation, IE’s document.cookie will not return a cookie if it was set with a path attribute containing a filename. (From Internet Explorer Cookie Internals (FAQ)).
+	 *  This means one cannot set a path using path: window.location.pathname in case such pathname contains a filename like so: /check.html (or at least, such cookie cannot be read correctly).
+	 */
     path?: string;
+	/** 
+	 *  Define the domain where the cookie is valid.
+	 *  Default: domain of page where the cookie was created.
+	 */
     domain?: string;
+	/**
+	 *  If true, the cookie transmission requires a secure protocol (https).
+	 *  Default: false.
+	 */
     secure?: boolean;
 }
 
@@ -17,7 +38,7 @@ interface JQueryCookieStatic {
     raw?: boolean;
     json?: boolean;
 
-    (): {[key:string]:string};
+    (): { [key: string]: string };
     (name: string): any;
     (name: string, converter: (value: string) => any): any;
     (name: string, value: string): void;
@@ -26,7 +47,7 @@ interface JQueryCookieStatic {
     (name: string, value: any, options: JQueryCookieOptions): void;
 }
 
-interface JQueryStatic {   
+interface JQueryStatic {
     cookie?: JQueryCookieStatic;
 
     removeCookie(name: string): boolean;
